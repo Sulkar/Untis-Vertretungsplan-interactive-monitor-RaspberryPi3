9fe5a27ab43fdb18322b603ea95487a21dc35f73
Steffen Benzinger <s.benzinger@msush.de>
2021-10-20 15:30:28 +0200

    update, tested Raspberry Pi OS 2021-05-07

--- RaspberryPi/myStart.sh
+++ RaspberryPi/myStart.sh
@@ -1,8 +1,11 @@
-#browser and python start
 echo "start unclutter in the background to remove the mouse pointer"
 unclutter &
+#unclutter -idle 0 &
+
 echo "Start python3 button script in the background"
 python3 /home/pi/ButtonControl.py &
-echo "Start midori in fullscreen after 3 seconds"
+
+echo "Start chromium-browser in fullscreen after 3 seconds"
 /bin/sleep 3
-midori -e Fullscreen -a http://vertretungsplan.meineSchule.de
+#chromium-browser --no-startup-window --kiosk --app=http://vertretungsplan.meineSchule.de
+chromium-browser --no-startup-window --start-fullscreen --start-maximized --app=http://vertretungsplan.meineSchule.de
